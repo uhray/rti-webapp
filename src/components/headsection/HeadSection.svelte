19ca4000841ba98fc47e31e4b905dfc6c327d8f9
Tyler Boyd <tboyd.evan@gmail.com>
2021-03-02 15:04:59 -0600

    building out head section component

--- src/components/headsection/HeadSection.svelte
+++ src/components/headsection/HeadSection.svelte
@@ -1,10 +1,13 @@
-<script></script>
+<script>
+  export let header;
+  export let subHeader;
+</script>
 
 <section class="HeadSection">
   <div class="HeadSection-container">
     <div class="HeadSection-content">
-      <h2 class="HeadSection-header">Some Stuff</h2>
-      <p class="HeadSection-subHeader">Some Other Stuff</p>
+      <h2 class="HeadSection-header">{header}</h2>
+      <p class="HeadSection-subHeader">{subHeader}</p>
     </div>
   </div>
 </section>
--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -2,6 +2,6 @@
   import HeadSection from '../../components/headsection/HeadSection.svelte';
 </script>
 
-<HeadSection />
+<HeadSection header="App Settings" subHeader="Change and customize the appearance of the driver’s application." />
 
 <style></style>
