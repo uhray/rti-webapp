8e6ee735cb80e92f0a8fdb5a726264568760c211
Tyler Boyd <tboyd.evan@gmail.com>
2021-03-02 16:58:49 -0600

    filling out settings page

--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,11 +1,33 @@
 <script>
+  import Card from '../../components/card/Card.svelte';
 </script>
 
 <section>
   <!-- Upload Logo Section -->
-  <p>I am a logo</p>
+  <h4>Upload your logo</h4>
+  <p>Recommended file types: .jpeg, .png, .svg, .tiff</p>
+  <div class="uk-flex">
+    <div class="Settings-card">
+      <Card
+        type="card-gray-border"
+        header="Some Header"
+        content="Some Content"
+      />
+    </div>
+    <div class="Settings-logoPreview">
+      <!-- Logo Preview -->
+      logopreview
+    </div>
+  </div>
+
   <!-- Change Theme Section -->
-  <p>I am a theme</p>
 </section>
 
-<style></style>
+<style lang="scss">
+  .Settings-card {
+    width: 35%;
+  }
+  .Settings-logoPreview {
+    width: 35%;
+  }
+</style>
