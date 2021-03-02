a7f7278b5e5a4fd08cafcb35dd87c9d5043785e9
Hugo Oliveira <hugo@uhray.com>
2021-03-02 15:24:48 -0600

    extra demo table

--- src/routes/components/index.svelte
+++ src/routes/components/index.svelte
@@ -102,6 +102,28 @@
       },
     ],
   };
+
+  const tableDataSmall = {
+    headers: [
+      { header: 'order', text: 'Order' },
+      { header: 'status', text: 'Status' },
+      { header: 'vehicle', text: 'Vehicle' },
+      { header: 'startPoint', text: 'Start Point' },
+      { header: 'endPoint', text: 'End Point' },
+      { header: 'view', text: 'View' },
+      { header: 'actions', text: 'Actions' },
+    ],
+    data: [
+      {
+        order: '0102031',
+        orderDate: 'Jan 23, 2020',
+        status: 'pending',
+        vehicle: '1XY001',
+        startPoint: 'Seattle, WA 02476',
+        endPoint: 'Seattle, WA 01652',
+      },
+    ],
+  };
 </script>
 
 <svelte:head>
@@ -116,6 +138,8 @@
 can set height based of vh units ( height={100} -> height: 100vh )
 defaults to [height: 100%] with no scrolling if no height value
 ------------------------------------------------------------------>
+<Table data={tableDataSmall} />
+<br />
 <Table data={tableData} height={50} />
 
 <h2>Divider</h2>
