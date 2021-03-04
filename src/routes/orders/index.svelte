99da1d8491bfbfc7853c71241191ade5a2baef80
Hugo Oliveira <hugo@uhray.com>
2021-03-04 09:20:30 -0600

    tweak table and set up orders page

---
+++ src/routes/orders/index.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Table from '../../components/table/Table.svelte';
+  import { data } from './data';
+</script>
+
+<style lang="scss">
+
+</style>
+
+<svelte:head>
+  <title>Orders</title>
+</svelte:head>
+
+<div class="Orders">
+  <br />
+  <br />
+  <br />
+  <Table {data} height={80} />
+</div>
