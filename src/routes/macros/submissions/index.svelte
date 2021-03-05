83e67a4191560faeced89539dd8e414247029725
Tyler Boyd <tboyd.evan@gmail.com>
2021-03-05 12:51:08 -0600

    added time off submissions page, subpage of macros

---
+++ src/routes/macros/submissions/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from './Header.svelte';
+</script>
+
+<Header />
+<section class="MacroSubmissionsPage">
+  <h3>Time Off Submissions</h3>
+</section>
+
+<style lang="scss">
+  .MacroSubmissionsPage {
+    padding: 2em;
+  }
+</style>
