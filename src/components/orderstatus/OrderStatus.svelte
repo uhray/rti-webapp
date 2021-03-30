26758e82271573ee8f2c5cf3c5f982348527af6c
Tyler Boyd <tboyd.evan@gmail.com>
2021-03-30 15:02:46 +0000

    adding some sub components for order status section

--- src/components/orderstatus/OrderStatus.svelte
+++ src/components/orderstatus/OrderStatus.svelte
@@ -1,9 +1,11 @@
 <script>
   import Card from '../card/Card.svelte';
   import TabMenu from '../tabmenu/TabMenu.svelte';
-  import Divider from '../divider/Divider.svelte';
   import Label from '../label/Label.svelte';
   import Icon from '../icon/Icon.svelte';
+  import TimelineSection from './sections/TimelineSection.svelte';
+  import CustomerSection from './sections/CustomerSection.svelte';
+  import ReferenceSection from './sections/ReferenceSection.svelte';
 
   export let orderStatusData = undefined;
   export let inProgress = true;
@@ -144,64 +146,12 @@
               type="default"
               tabs={['TIMELINE', 'CUSTOMER INFO', 'REFERENCE NUMBERS']}
             />
-            <div class="OrderStatus-timelineSection">
-              <div class="uk-flex">
-                <div class="uk-width-1-2">
-                  <h4 class="OrderStatus-timelineHeader">Trailer #</h4>
-                  <div class="OrderStatus-timelineContent">
-                    <p class="OrderStatus-timelineContentText">
-                      {orderStatusData.timeline.trailerNum}
-                    </p>
-                  </div>
-                </div>
-                <div class="uk-width-1-2">
-                  <h4 class="OrderStatus-timelineHeader">Event</h4>
-                  <div class="OrderStatus-timelineContent">
-                    <p class="OrderStatus-timelineContentText">
-                      {orderStatusData.timeline.event}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <Divider />
-              <div class="uk-flex">
-                <div class="uk-width-1-2">
-                  <h4 class="OrderStatus-timelineHeader">Appointment Time</h4>
-                  <div class="OrderStatus-timelineContent">
-                    <p class="OrderStatus-timelineContentText">
-                      {orderStatusData.timeline.apptTime}
-                    </p>
-                  </div>
-                </div>
-                <div class="uk-width-1-2">
-                  <h4 class="OrderStatus-timelineHeader">Arrival Time</h4>
-                  <div class="OrderStatus-timelineContent">
-                    <p class="OrderStatus-timelineContentText">
-                      {orderStatusData.timeline.arrivalTime}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <Divider />
-              <h4 class="OrderStatus-timelineHeader">Uploaded Documents</h4>
-              <div class="OrderStatus-timelineContent uk-flex">
-                <div class="OrderStatus-file">
-                  <Icon type="file" color="#a6adc4" />
-                </div>
-                <p class="OrderStatus-timelineContentText grayed">
-                  {orderStatusData.timeline.uploadedDocs}
-                </p>
-              </div>
-              <Divider />
-              <h4 class="OrderStatus-timelineHeader">
-                Driver / Delivery Notes
-              </h4>
-              <div class="OrderStatus-timelineContent">
-                <p class="OrderStatus-timelineContentText">
-                  {orderStatusData.timeline.deliveryNotes}
-                </p>
-              </div>
-            </div>
+            <!-- TIMELINE SECTION -->
+            <TimelineSection {orderStatusData} />
+            <!-- CUSTOMER INFO SECTION -->
+            <!-- <CustomerSection {orderStatusData} /> -->
+            <!-- REFERENCE SECTION -->
+            <!-- <ReferenceSection {orderStatusData} /> -->
           </div>
         </Card>
       </div>
