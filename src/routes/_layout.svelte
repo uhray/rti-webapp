e749b825fc6f8da7202463be7bed84a8522e55bd
Matt B <matt@uhray.com>
2021-07-15 17:02:00 -0500

    http

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -68,7 +68,7 @@
       return;
     }
 
-    let socket = io('https://truck-ops-api--dev.herokuapp.com', {
+    let socket = io('http://truck-ops-api--dev.herokuapp.com', {
       transports: ['websocket'],
       pingTimeout: 60000,
       query: { userId: userId },
