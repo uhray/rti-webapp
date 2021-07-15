dd94989d9909bcadf24b901031131c7ec1b648d1
Matt B <matt@uhray.com>
2021-07-15 17:31:19 -0500

    sockets api instance, port 8000

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -68,7 +68,7 @@
       return;
     }
 
-    let socket = io('https://truck-ops-api--dev.herokuapp.com', {
+    let socket = io('http://truck-ops-api-sockets--dev.herokuapp.com:8000', {
       transports: ['websocket'],
       pingTimeout: 60000,
       query: { userId: userId },
